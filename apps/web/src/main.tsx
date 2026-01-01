0b42853cdebd6261aad170eadcdc11b235712365
Phil Kallos <phil@neonredwood.com>
2025-12-31 20:01:06 -0800

    feat: add dark mode support to dashboard

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,6 +3,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "@/App";
+import { theme } from "@/theme";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -16,7 +17,7 @@ const queryClient = new QueryClient({
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ChakraProvider>
+      <ChakraProvider theme={theme}>
         <App />
       </ChakraProvider>
     </QueryClientProvider>
